e57f6010db4f6f37f262e8387148c1af60ad97c1
Jose Manuel Comber <jmcomber1@gmail.com>
2019-04-21 13:20:51 -0400

    feat(offer): added offer and feed components

---
+++ src/components/Feed.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>Ofertas</h1>
+    <br>
+    <div class="columns">
+      <div class="column">
+        <Offer
+          v-for="offer in offers"
+          :key="offer.title"
+          :offer="offer"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+import Offer from './Offer.vue'
+
+export default {
+  name: 'Feed',
+  components: {
+    Offer,
+  },
+  props: {
+    offers: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+    //   offers: [],
+    }
+  },
+  computed: {
+    items() {
+      return Object.keys(this.offers)
+        .map(
+          key => Object.assign({}, this.offers[key]),
+        )
+    },
+    ...mapState('offers', [
+      'offers',
+    ]),
+  },
+  created() {
+  },
+  methods: {
+    ...mapActions('users', [
+      'fetchOffers',
+    ]),
+  },
+}
+</script>
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
---
+++ src/components/Offer.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="offer">
+    <v-card>
+      <v-card-title>
+        <div>
+          <span class="offerTitle"><a
+            href="#"
+            class="offerInstitutionLink"
+          > {{ offer.institutionName }}</a> - {{ offer.title }}</span><br>
+          <span class="grey--text">{{ offer.description }}</span><br>
+          <span class="grey--text">Horas semanales: {{ offer.hoursPerWeek }}</span><br>
+          <span class="grey--text">Contactar a {{ offer.contactEmail }}</span>
+        </div>
+      </v-card-title>
+      <v-card-actions>
+        <v-btn
+          flat
+          color="orange"
+        >
+          Explore
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+// import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'Offer',
+  props: {
+    offer: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+    //   offer: {},
+    }
+  },
+  computed: {
+  },
+  created() {
+  },
+  methods: {
+  },
+}
+</script>
+<style scoped>
+  .offerTitle {
+      font-size: 24px;
+  }
+  .offerInstitutionLink {
+      text-decoration: none;
+      color: black;
+  }
+</style>
