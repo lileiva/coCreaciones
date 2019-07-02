4f1f44dc3d431467d115da81cb5ff3c4ed09a6d1
Nico <nlacosta@uc.cl>
2019-07-02 18:46:28 -0400

    front page fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import Vuetify from 'vuetify'
 import es from 'vuetify/es5/locale/es'
 import App from './App.vue'
@@ -7,7 +8,10 @@ import store from './store'
 import './registerServiceWorker'
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue)
 
 Vue.use(Vuetify, {
   lang: {
