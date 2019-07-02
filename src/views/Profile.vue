9b8303d50513889e3bcf6da5395f431be332b30c
Nico <nlacosta@uc.cl>
2019-07-02 19:59:13 -0400

    base profile

---
+++ src/views/Profile.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="about">
+    <v-layout
+      row
+      wrap
+    >
+      <v-flex xs3>
+        <v-card class="orgCard">
+          <v-card-title primary-title>
+            <div class="show_content">
+              <h3 class="headline mb-0">
+                Juanito Perez
+              </h3>
+              <br>
+
+              <br>
+              <div>
+                Contacto
+              </div><br>
+              <div>
+                Cel  +56964965470 <br>
+                Mail j.perez@gmail.com <br>
+              </div>
+              <div>
+                Cel  +56964965470 <br>
+                Mail j.perez@gmail.com <br>
+              </div>
+              <div>
+                Cel  +56964965470 <br>
+                Mail j.perez@gmail.com <br>
+              </div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+      <div>
+        Soy juanito Perez y me encanta el latin
+
+        Ut in dolor sed sem porttitor porttitor. Duis consequat vel sapien ac placerat.
+        Cras gravida felis purus. Praesent cursus orci non ex ultrices pretium.
+        Maecenas mollis varius tortor eget faucibus. Donec non justo sit amet
+        turpis aliquam sagittis. Sed tempor mollis fermentum. Nunc vel felis
+        mollis, tempor neque vel, tincidunt erat. Morbi non tortor porta,
+        sodales nulla eget, molestie enim. Sed interdum sagittis sem, sit
+        amet scelerisque ante ultrices et. Nam rhoncus gravida felis, quis
+        vestibulum mauris pretium non. Suspendisse potenti.
+      </div>
+    </v-layout>
+    name
+    Contacto
+    Keywords
+    Quien es
+    Experiencia autobiografica
+    Validaciones de instituciones
+    Validaciones de personas
+  </div>
+</template>
+<script>
+export default {
+  name: 'Profile',
+}
+</script>
